ab89871172b576535d3fccf09fc9df834dda9384
albert <yamov230@gmail.com>
2018-06-08 22:14:02 +0300

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+const gulp = require('gulp');
+const pug = require('gulp-pug');
+
+const paths = {
+    root: './build',
+    templates: {
+        pages: 'src/views/pages/*.pug',
+        src: 'src/views/**/*.pug',
+        dest: 'build'
+    }
+}
+
+//pug
+function templates() {
+    return gulp.src(paths.templates.pages)
+        .pipe(pug({ pretty: true}))
+        .pipe(gulp.dest(paths.root));
+}
+
+exports.templates = templates;
